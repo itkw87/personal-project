d4cff21b0027ddd2d2a0563cc702ef6067a7fd5a
itkw87 <itkw87@naver.com>
2023-07-06 21:01:55 +0900

    client/common/server 프로젝트 백업 파일 추가

---
+++ app37-common/src/main/java/personal/project/dao/BoardDao.java
@@ -0,0 +1,16 @@
+package personal.project.dao;
+
+import java.util.List;
+import personal.project.vo.Board;
+
+public interface BoardDao {
+  void insert(Board board);
+
+  List<Board> list();
+
+  Board findBy(int no);
+
+  int update(Board board);
+
+  int delete(int no);
+}
