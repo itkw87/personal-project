44ae7b50e9b204c2fdf7dc81f49b21446ed57ffa
itkw87 <itkw87@naver.com>
2023-07-06 20:49:16 +0900

    개인프로젝트 Client/Server System 아키텍처 적용

---
+++ app-server/src/main/java/personal/project/handler/StudentUpdateListener.java
@@ -0,0 +1,36 @@
+package personal.project.handler;
+
+import personal.project.dao.StudentDao;
+import personal.project.vo.Student;
+import personal.util.BreadcrumbPrompt;
+
+public class StudentUpdateListener implements StudentActionListener {
+  StudentDao studentDao;
+
+  public StudentUpdateListener(StudentDao studentDao) {
+    this.studentDao = studentDao;
+  }
+
+  @Override
+  public void service(BreadcrumbPrompt prompt) {
+    int studentNo = prompt.inputInt("번호? ");
+    Student s = studentDao.findBy(studentNo);
+
+    if (s == null) {
+      System.out.println("해당 번호의 학생이 없습니다!");
+      return;
+    }
+
+    s.setBirth(prompt.inputInt("생년월일(%d)? ", s.getBirth()));
+    s.setName(prompt.inputString("이름(%s)? ", s.getName()));
+    s.setGrade(prompt.inputInt("학년(%d)? ", s.getGrade()));
+    s.setKoreanScore(prompt.inputInt("국어점수(%d)? ", s.getKoreanScore()));
+    s.setEnglishScore(prompt.inputInt("영어점수(%d)? ", s.getEnglishScore()));
+    s.setMathScore(prompt.inputInt("수학점수(%d)? ", s.getMathScore()));
+    s.setStatus(StudentActionListener.inputStatus(s.getStatus(), "update", prompt));
+    s.setGender(StudentActionListener.inputGender(s.getGender(), prompt));
+    s.setScoreAvg((float) (s.getKoreanScore() + s.getEnglishScore() + s.getMathScore()) / 3);
+
+    studentDao.update(s);
+  }
+}
